2dac274d93760da831bc5520b9b06d303bffab42
Jakub Górski <242396@edu.p.lodz.pl>
2023-04-13 00:51:19 +0200

    View-ViewModel increment/subtract buttons binded Commands  and _numberOfBalls

---
+++ ViewModel/SimulationViewModel.cs
@@ -0,0 +1,45 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class SimulationViewModel : ViewModelBase
+    {
+
+        private int _numberOfBalls = 5;
+
+        public SimulationViewModel()
+        {
+            IncrementBallNumberCommand = new RelayCommand(IncrementBallNumber);
+            SubtractBallNumberCommand = new RelayCommand(SubtractBallNumber);
+        }
+
+        public ICommand IncrementBallNumberCommand { get; }
+        public ICommand SubtractBallNumberCommand { get; }
+
+        public int NumberOfBalls
+        {
+            get { return _numberOfBalls; }
+            set
+            {
+                _numberOfBalls = value;
+                OnPropertyChanged(nameof(NumberOfBalls));
+            }
+        }
+
+        private void IncrementBallNumber()
+        {
+            NumberOfBalls++;
+        }
+
+        private void SubtractBallNumber()
+        {
+            NumberOfBalls--;
+        }
+    }
+}
