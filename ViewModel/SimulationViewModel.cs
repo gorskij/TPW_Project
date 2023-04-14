7e937a4c98342cf7efef3c0ea5dbf38c37ebe6b0
Jakub Górski <242396@edu.p.lodz.pl>
2023-04-14 18:19:01 +0200

    Start & Stop buttons in view binded to viewModel

--- ViewModel/SimulationViewModel.cs
+++ ViewModel/SimulationViewModel.cs
@@ -12,15 +12,32 @@ namespace ViewModel
     {
 
         private int _numberOfBalls = 5;
+        private bool _isRunning = false;
+        private bool _isStartEnabled = true;
+        private bool _isStopEnabled = false;
 
         public SimulationViewModel()
         {
             IncrementBallNumberCommand = new RelayCommand(IncrementBallNumber);
             SubtractBallNumberCommand = new RelayCommand(SubtractBallNumber);
+            StartCommand = new RelayCommand(Start);
+            StopCommand = new RelayCommand(Stop);
         }
 
         public ICommand IncrementBallNumberCommand { get; }
         public ICommand SubtractBallNumberCommand { get; }
+        public ICommand StopCommand { get; }
+        public ICommand StartCommand { get; }
+
+        public bool IsRunning
+        {
+            get { return _isRunning; }
+            set {
+                if (value == _isRunning) return;
+                _isRunning = value;
+                OnPropertyChanged(nameof(IsRunning));
+            }
+        }
 
         public int NumberOfBalls
         {
@@ -32,6 +49,28 @@ namespace ViewModel
             }
         }
 
+        public bool IsStartEnabled
+        {
+            get { return _isStartEnabled; }
+            set
+            {
+                if (value == _isStartEnabled) return;
+                _isStartEnabled = value;
+                OnPropertyChanged(nameof(IsStartEnabled));
+            }
+        }
+
+        public bool IsStopEnabled
+        {
+            get { return _isStopEnabled; }
+            set
+            {
+                if (value == _isStopEnabled) return;
+                _isStopEnabled = value;
+                OnPropertyChanged(nameof(IsStopEnabled));
+            }
+        }
+
         private void IncrementBallNumber()
         {
             NumberOfBalls++;
@@ -40,6 +79,20 @@ namespace ViewModel
         private void SubtractBallNumber()
         {
             NumberOfBalls--;
+        }
+
+        public void Start()
+        {
+            IsStartEnabled = false;
+            IsStopEnabled = true;
+            IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            IsStopEnabled = false;
+            IsStartEnabled = true;
+            IsRunning = false;
         }
     }
 }
